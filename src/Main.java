964e68631a702ed17354f45889d51d6ceaae962f
Gabriel <gabrielfg12345@gmail.com>
2025-04-02 16:39:28 -0300

    testando ---------------------- ---------------------- Ordem-Servico: SGPS-#9999999

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        for(int linha = 0; linha < 10; linha++) {
-            for(int coluna = 0; coluna < 9; coluna++) {
-                System.out.print("*");
+        for(int linha = 0; linha < 11; linha++) {
+            for(int coluna = 0; coluna < 10; coluna++) {
+                System.out.print("a");
             }
             System.out.println();
         }
